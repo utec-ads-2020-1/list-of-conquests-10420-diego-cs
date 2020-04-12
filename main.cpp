7839bfd9d6380c72cefd7ee334d335240885c778
Diego <diego@archlinux.localdomain>
2020-04-12 12:21:49 -0500

    Solution with std::map

--- main.cpp
+++ main.cpp
@@ -7,19 +7,18 @@ int main() {
     int n;
     string country;
     string woman;
-    map<string, int> country_count;
+    map<string, int> count;
     
     scanf("%d", &n);
     for (int i = 0; i < n; ++i) {
         cin >> country;
         getline(cin, woman); // reads remainder string with spaces (fullname woman loved)
-        country_count[country]++;
+        count[country]++;
     }
 
     map<string, int>::iterator it;
-    for (it = country_count.begin(); it != country_count.end(); ++it) {
+    for (it = count.begin(); it != count.end(); ++it)
         cout << it->first << " " << it->second << endl;
-    }
 
     return 0;
 }
